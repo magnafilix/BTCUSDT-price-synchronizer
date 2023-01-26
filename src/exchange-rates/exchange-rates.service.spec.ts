540d328b7cb125cea67204fa2d2c881ae4092358
magnafilix <denyz.pylypenko@gmail.com>
2023-01-26 19:59:57 +0200

    test(): [unit] exchange rates current rate

---
+++ src/exchange-rates/exchange-rates.service.spec.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ExchangeRatesService } from './exchange-rates.service';
+import { ExchangeRateEntity } from './entities/exchange-rate.entity';
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+  }),
+);
+
+describe('ExchangeRatesService', () => {
+  let service: ExchangeRatesService;
+  let repositoryMock: MockType<Repository<ExchangeRateEntity>>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ExchangeRatesService,
+        {
+          provide: getRepositoryToken(ExchangeRateEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
+    }).compile();
+
+    service = module.get<ExchangeRatesService>(ExchangeRatesService);
+    repositoryMock = module.get(getRepositoryToken(ExchangeRateEntity));
+  });
+
+  it('should get current exchange rate', async () => {
+    const currentExchangeRate = {
+      id: 258,
+      price: '23015.00000000',
+      symbol: 'BTCUSDT',
+    };
+
+    repositoryMock.findOne.mockReturnValue(currentExchangeRate);
+
+    expect(await service.getCurrentExchangeRate()).toEqual(currentExchangeRate);
+  });
+});
